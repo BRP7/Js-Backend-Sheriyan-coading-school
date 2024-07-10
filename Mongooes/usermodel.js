0d32f4fc97085e87f4fd9a34d48c0a10164c3ca2
BRP7 <bhumiparmar0007@gmail.com>
2024-07-10 22:07:19 +0530

    Basic Route Creation

--- Mongooes/usermodel.js
+++ Mongooes/usermodel.js
@@ -1,11 +1,24 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/mongopractice")
+mongoose.connect("mongodb://127.0.0.1:27017/mongopractice", {
+  // Remove useNewUrlParser and useUnifiedTopology options
+  // They are no longer needed and deprecated in newer versions
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("MongoDB connected");
+})
+.catch(err => {
+  console.error("MongoDB connection error:", err);
+});
 
 const userSchema = mongoose.Schema({
-    name:String,
-    username:String,
-    email:String
-})
+    name: String,
+    username: String,
+    email: String
+});
 
-module.exports = mongoose.model("user",userSchema)
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
