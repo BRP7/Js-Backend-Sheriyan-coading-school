7c95a842343990400a97fccf072e78966c1041cb
BRP7 <bhumiparmar0007@gmail.com>
2024-07-30 11:50:41 +0530

    set up ejs

--- 03_BasicPostApp/app.js
+++ 03_BasicPostApp/app.js
@@ -7,10 +7,14 @@ const path = require("path")
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home Page' });
-    // res.send("hello");
+    const posts = [
+        { id: 1, image: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', username: 'user1', content: 'How Beautiful !!!!!' },
+        { id: 2, image: 'https://images.pexels.com/photos/24300078/pexels-photo-24300078/free-photo-of-blue-suv-with-open-driver-door.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', username: 'user2', content: 'This is the second post' },
+    ];
+    res.render('index', { title: 'Home Page', posts });
 });
 
 app.listen(3000, () => {
